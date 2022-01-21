2de8368d14389ef5e0913a65e7c11584a4609277
Spartak033 <98089448+Spartak033@users.noreply.github.com>
2022-01-21 08:56:49 +0200

    commit

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -5,14 +5,30 @@ namespace GitExercise
     public static class OptionsManager
     {
         public static string[] OptionsList = {
-            "a - Add"
+            "a - Add",
+            "s - Subtract",
+            "m - Multyply",
+            "d - Divide"
         };
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
 
-       
+        }
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+
+
     }
 }
